0d3f9e02e97e940c124f4ff5947cf3a31f89121d
abekem <nek.7180@gmail.com>
2023-03-15 00:56:15 +0900

    OSごとの設定ファイルを追加

---
+++ fish/config-osx.fish
@@ -0,0 +1,1 @@
+set DOCKER_HOST unix://$HOME/docker.sock
--- fish/config.fish
+++ fish/config.fish
@@ -4,3 +4,17 @@ end
 
 export LSCOLORS=Gxfxcxdxbxegedabagacad
 
+switch (uname)
+  case Darwin
+    source (dirname (status --current-filename))/config-osx.fish
+  case Linux
+    source (dirname (status --current-filename))/config-linux.fish
+  case '*'
+    source (dirname (status --current-filename))/config-windows.fish
+end
+
+set LOCAL_CONFIG (dirname (status --current-filename))/config-local.fish
+if test -f $LOCAL_CONFIG
+  source $LOCAL_CONFIG
+end
+

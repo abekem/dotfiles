0a32cd5f92f4b15e4aec9b176d2d2308e042d661
abekem <nek.7180@gmail.com>
2023-03-31 18:11:25 +0900

    [nvim] add filetype settings

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,7 +3,7 @@ if not vim.g.vscode then
   vim.opt.number = true
   vim.opt.relativenumber = true
   -- 日本語の文字コード対応
-  vim.opt.ambiwidth="single"
+  vim.opt.ambiwidth = "single"
   vim.opt.shiftwidth = 4
   vim.opt.smarttab = true
   vim.opt.expandtab = true
@@ -11,6 +11,14 @@ if not vim.g.vscode then
   vim.opt.fileencodings = "ucs-boms,utf-8,euc-jp,cp932"
   -- 改行コードの自動判別. 左側が優先される
   vim.opt.fileformats = "unix,dos,mac"
+
+  local filetypes = require('filetypes')
+  vim.api.nvim_create_augroup('vimrc_augroup', {})
+  vim.api.nvim_create_autocmd('FileType', {
+    group = 'vimrc_augroup',
+    pattern = '*',
+    callback = function(args) filetypes[args.match]() end
+  })
 end
 
 -- undoを永続化
---
+++ nvim/lua/filetypes.lua
@@ -0,0 +1,35 @@
+if vim.g.vscode then return end
+local M = {}
+
+local function set_indent(tab_length, is_hard_tab)
+  if is_hard_tab then
+    vim.bo.expandtab = false
+  else
+    vim.bo.expandtab = true
+  end
+
+  vim.bo.shiftwidth  = tab_length
+  vim.bo.softtabstop = tab_length
+  vim.bo.tabstop     = tab_length
+end
+
+M.help = function()
+  vim.api.nvim_buf_set_keymap(0, 'n', 'q', 'ZZ', { noremap = true })
+end
+
+M.java = function()
+  set_indent(4, false)
+end
+
+M.lua = function()
+  set_indent(2, false)
+end
+
+return setmetatable(M, {
+  __index = function()
+    return function()
+      print('Unexpected filetype!')
+      set_indent(2, false)
+    end
+  end
+})

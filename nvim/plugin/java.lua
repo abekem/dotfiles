51c8d8d8dd30babe3c93a0903eae17634a736367
abe.kentaro <abe.kentaro@zynas.co.jp>
2023-04-06 09:12:38 +0900

    [nvim] fix windows command path

--- nvim/plugin/java.lua
+++ nvim/plugin/java.lua
@@ -4,14 +4,9 @@ if (not status) then return end
 
 local config
 if vim.fn.has "macunix" == 1 then return end
---config = {
---  cmd = { vim.env.MASON .. '/bin/jdtls' },
---  root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', '.git', 'mvnw' }, { upward = true })[1]),
---}
---end
 if vim.fn.has "win32" == 1 then
   config = {
-    -- cmd = { '/path/to/jdt-language-server/bin/jdtls' },
+    cmd = { vim.env.MASON .. '\\bin\\jdtls.cmd' },
     root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', '.git', 'mvnw' }, { upward = true })[1]),
   }
 end

5b99506d55098c522b63878846d0a399c0801f46
abekem <nek.7180@gmail.com>
2023-04-08 03:28:20 +0900

    [nvim] add spell check

--- nvim/plugin/null-ls.rc.lua
+++ nvim/plugin/null-ls.rc.lua
@@ -13,15 +13,25 @@ local lsp_formatting = function(bufnr)
   })
 end
 
+local sources = {
+  null_ls.builtins.formatting.prettierd,
+  null_ls.builtins.diagnostics.eslint_d.with({
+    diagnostics_format = '[eslint] #{m}\n(#{c})'
+  }),
+  null_ls.builtins.diagnostics.fish,
+  --null_ls.builtins.formatting.google_java_format,
+  null_ls.builtins.diagnostics.cspell.with({
+    diagnostics_postprocess = function(diagnostic)
+      diagnostic.severity = vim.diagnostic.severity["WARN"]
+    end,
+    condition = function()
+      return vim.fn.executable('cspell') > 0
+    end
+  })
+}
+
 null_ls.setup {
-  sources = {
-    null_ls.builtins.formatting.prettierd,
-    null_ls.builtins.diagnostics.eslint_d.with({
-      diagnostics_format = '[eslint] #{m}\n(#{c})'
-    }),
-    null_ls.builtins.diagnostics.fish,
-    --null_ls.builtins.formatting.google_java_format,
-  },
+  sources = sources,
   on_attach = function(client, bufnr)
     if client.supports_method("textDocument/formatting") then
       vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })

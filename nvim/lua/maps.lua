d203edd144a5df51e33e94a0d9d703228de2380e
abe.kentaro <abe.kentaro@zynas.co.jp>
2023-04-08 23:11:22 +0900

    [nvim] add some keymap

--- nvim/lua/maps.lua
+++ nvim/lua/maps.lua
@@ -11,6 +11,10 @@ keymap.set('n', '<C-a>', 'gg<S-v>G')
 
 -- New tab
 keymap.set('n', 'te', ':tabedit')
+-- Move Tab
+keymap.set('n', 'tm', ':tabmove')
+-- Close Current Tab
+keymap.set('n', 'tc', ':tabclose<Return>')
 -- Split window
 keymap.set('n', 'ss', ':split<Return><C-w>w')
 keymap.set('n', 'sv', ':vsplit<Return><C-w>w')
@@ -20,6 +24,10 @@ keymap.set('', 'sh', '<C-w>h')
 keymap.set('', 'sk', '<C-w>k')
 keymap.set('', 'sj', '<C-w>j')
 keymap.set('', 'sl', '<C-w>l')
+-- Move Buffer
+keymap.set('n', 'bn', ':bn<Return>')
+keymap.set('n', 'bp', ':bp<Return>')
+keymap.set('n', 'bm', ':bm<Return>')
 
 -- Escape
 keymap.set('i', 'jj', '<Esc>')

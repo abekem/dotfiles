c78cb1dbf800617a31cee3414d5d1735a10a486c
abekem <nek.7180@gmail.com>
2023-07-16 14:32:54 +0900

    [nvim] update im-select config

--- nvim/after/plugin/im_select.rc.lua
+++ nvim/after/plugin/im_select.rc.lua
@@ -10,17 +10,17 @@ if vim.fn.has "macunix" == 1 then
     -- For macOS, default: "com.apple.keylayout.ABC", aka: US
     -- For Linux, default: "keyboard-us"
     -- You can use `im-select` or `fcitx5-remote -n` to get the IM's name you preferred
-    default_im_select    = "com.apple.keylayout.ABC",
+    default_im_select   = "com.apple.keylayout.ABC",
 
     -- Set to 1 if you don't want restore IM status when `InsertEnter`
-    disable_auto_restore = 1,
+    set_previous_events = {},
 
     -- Can be binary's name or binary's full path,
     -- e.g. 'im-select' or '/usr/local/bin/im-select'
     -- For Windows/WSL, default: "im-select.exe"
     -- For macOS, default: "im-select"
     -- For Linux, default: "fcitx5-remote"
-    default_command      = 'im-select'
+    default_command     = 'im-select'
   }
 end
 if vim.fn.has "win32" == 1 then

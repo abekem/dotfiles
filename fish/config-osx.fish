6c85f01e7c11eeb480b53ac33ea2bc917ac62edd
abekem <26917919+abekem@users.noreply.github.com>
2026-03-20 11:43:34 +0900

    既存の設定ファイルを削除

--- fish/config-osx.fish
+++
@@ -1,6 +0,0 @@
-fish_add_path ~/Commands
-
-### MANAGED BY RANCHER DESKTOP START (DO NOT EDIT)
-set --export --prepend PATH "/Users/abekem/.rd/bin"
-### MANAGED BY RANCHER DESKTOP END (DO NOT EDIT)
-
--- nvim/after/plugin/textcase.rc.lua
+++
@@ -1,6 +0,0 @@
-local status, textcase = pcall(require, "textcase")
-if (not status) then return end
-
-textcase.setup({})
-
-vim.keymap.set('n', 'gas', '<Cmd>lua require(\'textcase\').current_word(\'to_snake_case\')<CR>')

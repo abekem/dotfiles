2304f9dc277dce7ae9f46269fc96094257019265
abekem <26917919+abekem@users.noreply.github.com>
2026-09-07 14:41:54 +0900

    Remove .config/nvim/after/plugin/autopairs.rc.lua Remove .config/nvim/after/plugin/bufferline.rc.lua Remove .config/nvim/after/plugin/cmp.rc.lua Remove .config/nvim/after/plugin/colorizer.rc.lua Remove .config/nvim/after/plugin/comment.rc.lua Remove .config/nvim/after/plugin/git.rc.lua Remove .config/nvim/after/plugin/gitsigns.rc.lua Remove .config/nvim/after/plugin/hlslens.rc.lua Remove .config/nvim/after/plugin/im_select.rc.lua Remove .config/nvim/after/plugin/indent.rc.lua Remove .config/nvim/after/plugin/lsp-colors.rc.lua Remove .config/nvim/after/plugin/lualine.rc.lua Remove .config/nvim/after/plugin/neosolarized.rc.lua Remove .config/nvim/after/plugin/pounce.rc.lua

--- dot_config/nvim/after/plugin/gitsigns.rc.lua
+++
@@ -1,7 +0,0 @@
-if vim.g.vscode then return end
-local status, gitsigns = pcall(require, "gitsigns")
-if (not status) then return end
-
-gitsigns.setup {
-  current_line_blame = true,
-}
--- dot_config/nvim/after/plugin/pounce.rc.lua
+++
@@ -1,9 +0,0 @@
-local status, pounce = pcall(require, "pounce")
-if (not status) then return end
-
-local map = vim.keymap.set
-map("n", "s", function() pounce.pounce {} end)
-map("n", "S", function() pounce.pounce { do_repeat = true } end)
-map("x", "s", function() pounce.pounce {} end)
-map("o", "gs", function() pounce.pounce {} end)
-map("n", "S", function() pounce.pounce { input = { reg = "/" } } end)

d28c8c9ffef18e943d85c5ea300f942715d5d88d
abekem <26917919+abekem@users.noreply.github.com>
2026-09-15 15:56:39 +0900

    Update .config/fish/config-osx.fish

--- dot_config/private_fish/config-osx.fish
+++ dot_config/private_fish/config-osx.fish
@@ -4,7 +4,8 @@ fish_add_path ~/Commands
 set --export --prepend PATH "/Users/abekem/.rd/bin"
 ### MANAGED BY RANCHER DESKTOP END (DO NOT EDIT)
 
-set --export SSH_AUTH_SOCK /Users/abekem/Library/Containers/com.bitwarden.desktop/Data/.bitwarden-ssh-agent.sock
+# set --export SSH_AUTH_SOCK /Users/abekem/Library/Containers/com.bitwarden.desktop/Data/.bitwarden-ssh-agent.sock
+set --export SSH_AUTH_SOCK /Users/abekem/.bitwarden-ssh-agent.sock
 
 fish_add_path /opt/homebrew/bin
 
